cf8e0b1aa2b44157f524c1e6e7d174109311d20e
hellorealworld <hellorealworld@163.com>
2023-05-25 17:58:35 +0800

    更新了晚上的作业

---
+++ musicobj02/src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:3000/"
+
+export function getBanner (type=0) {
+    return axios.get(baseUrl + "banner?type=" + type)
+}
+
+
+// export default { getBanner }
--- musicobj02/src/components/SwiperCom.vue
+++ musicobj02/src/components/SwiperCom.vue
@@ -1,15 +1,78 @@
 <template>
-    <div>
-        我是 swiper
+    <div id="swipercom">
+        <div class="swiper-container" id="swiperIndex">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="(item,index) in swlist" :key="index"><img :src="item.pic" :alt="item.title"></div>
+            </div>
+            <div class="swiper-pagination">
+
+            </div>
+        </div>
     </div>
 </template>
 
+
 <script>
+//导入js
+import Swiper from 'swiper'
+//引入css
+import "swiper/css/swiper.min.css"
+
+import {getBanner} from '@/api/index'
+
 export default{
-    name:'swiper'
+    name:'swiper',
+    data() {
+        return {
+            swlist:[
+                {
+                    pic: require("../assets/logo.png")
+                },{
+                    pic: require("../assets/logo.png")
+                },{
+                    pic: require("../assets/logo.png")
+                },{
+                    pic: require("../assets/logo.png")
+                },{
+                    pic: require("../assets/logo.png")
+                },{
+                    pic: require("../assets/logo.png")
+                }
+            ],
+        }
+    },
+    async mounted() {
+        var res = await getBanner(1);
+        console.log(res)
+        this.swlist=res.data.banners
+        console.log(this.swlist)
+        var myswiper = new Swiper("#swiperIndex",{
+            loop:true,
+            pagination:{
+                el:".swiper-pagination",
+                clickable:true
+            }
+        })
+
+    }
 }
 </script>
 
-<style>
-
+<style lang="less">
+    #swipercom{
+        width: 7.5rem;
+    #swiperIndex.swiper-container{
+        width: 7.1rem;
+        height: 2.6rem;
+        border-radius: 0.1rem;
+        
+        .swiper-slide img{
+            width: 100%;
+        }
+        .swiper-pagination-bullet-active{
+            background-color: orangered;
+        }
+    
+    }
+}
 </style>
